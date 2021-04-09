245dc0d3db0cc01337975aee906cd9359cf6b465
topcueser <topcueser@gmail.com>
2021-04-10 00:53:42 +0300

    Startup kalabalık olmasın - extension yazalım

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using Core.Interfaces;
@@ -27,42 +28,19 @@ namespace API
         {
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
-            services.AddScoped<IProductRepository,ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
             services.AddAutoMapper(typeof(MappingProfiles));
 
-            //Request validation icin ozellestirmeler
-            services.Configure<ApiBehaviorOptions>(options => 
-            {
-                options.InvalidModelStateResponseFactory = actionContext => 
-                {
-                    var errors = actionContext.ModelState
-                        .Where(e => e.Value.Errors.Count > 0)
-                        .SelectMany(x => x.Value.Errors)
-                        .Select(x => x.ErrorMessage).ToArray();
-
-                    var errorResponse = new ApiValidationError
-                    {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
+            // Startup kalabalık olmasın. Kendimiz extension yazarak onun içine dahil ettik.
+            services.AddApplicationServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             // custom olarak oluşturduğumuz exception sınıfı üzerinden hatalar gösterilecek
-            // IsDevelopment() içerisindeki default hatayı kapattık
+            // IsDevelopment() içerisindeki default hata olan app.UseDeveloperExceptionPage() kapattık ve if (env.IsDevelopment()) ı kaldırdık
             app.UseMiddleware<ExceptionMiddleware>();
-            if (env.IsDevelopment())
-            {
-                // yukarıda özelleştirilmiş olan ExceptionMiddleware kullanılıyor.
-                //app.UseDeveloperExceptionPage();
-            }
-
+            
             app.UseRouting();
             app.UseStaticFiles();
 
